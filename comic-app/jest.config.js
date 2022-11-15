9c00a84c2290e802cc82fc313f03bda033fa5051
katesivy <katesivy@gmail.com>
2022-11-15 14:33:08 -0500

    add unversioned files

---
+++ comic-app/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    presets: [
+      // ['@babel/preset-env', {targets: {node: 'current'}}],
+      ['@babel/preset-env'],
+      '@babel/preset-typescript',
+    ],
+    rules: [
+      // This would match almost any react-native module
+      {
+        test: /(@?react-(navigation|native)).*\.(ts|js)x?$/,
+        include: /node_modules/,
+        exclude: [/react-native-web/, /\.(native|ios|android)\.(ts|js)x?$/],
+        loader: 'babel-loader'
+      },
+      // This would match ui-kitten
+      {
+          test: /@?(ui-kitten|eva-design).*\.(ts|js)x?$/,
+          loader: 'babel-loader'
+      }
+    ],
+    transform: {
+      '^.+\\.(ts|tsx)?$': 'ts-jest',
+      "^.+\\.(js|jsx)$": "babel-jest",
+    }
+  };
