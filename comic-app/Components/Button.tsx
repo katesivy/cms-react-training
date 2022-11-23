e7d35d7662646229679dde96ab420df3c3b7a0e1
katesivy <katesivy@gmail.com>
2022-11-23 11:00:29 -0500

    added Button.tsx

---
+++ comic-app/Components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import '@fortawesome/fontawesome-svg-core/styles.css'
+// import { faBolt } from '@fortawesome/free-solid-svg-icons'
+import styles from '../styles/Button.module.css'
+
+export default function Button() {
+
+    return (
+
+        <div className={styles.iconContainer}>
+            {/* <FontAwesomeIcon className={styles.icon} icon={faBolt} /> */}
+        </div>
+    )
+}
